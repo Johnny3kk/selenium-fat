abc36a65a22ab9c17be709735cea1a536e4fb913
Johnny3kk <some@mail.ru>
2022-03-21 19:56:55 +0300

    Allure settings;

---
+++ src/main/java/ru/ibs/framework/managers/DriverManager.java
@@ -0,0 +1,50 @@
+package ru.ibs.framework.managers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.ibs.framework.utils.PropsConst;
+
+
+public class DriverManager {
+
+    private WebDriver driver;
+    private TestPropManager propManager = TestPropManager.getInstance();
+
+    private static DriverManager INSTANCE = null;
+
+
+    private DriverManager() {}
+
+    public static DriverManager getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new DriverManager();
+        }
+        return INSTANCE;
+    }
+
+    public WebDriver getDriver() {
+        if (driver == null) {
+            initDriver();
+        }
+        return driver;
+    }
+
+    private void initDriver() {
+        if (propManager.getProperty(PropsConst.TYPE_BROWSER).equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", propManager.getProperty(PropsConst.PATH_GECKO_DRIVER_WINDOWS));
+            driver = new FirefoxDriver();
+        } else if (propManager.getProperty(PropsConst.TYPE_BROWSER).equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", propManager.getProperty(PropsConst.PATH_CHROME_DRIVER_WINDOWS));
+            driver = new ChromeDriver();
+        }
+    }
+
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+}
