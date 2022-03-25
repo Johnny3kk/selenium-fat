e206cc9e68539b1f4a45116d256a14b8e742ec80
Johnny3kk <some@mail.ru>
2022-03-25 22:48:50 +0300

    selenoid remote builtin;

--- src/main/java/ru/ibs/framework/managers/DriverManager.java
+++ src/main/java/ru/ibs/framework/managers/DriverManager.java
@@ -3,7 +3,12 @@ package ru.ibs.framework.managers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.ibs.framework.utils.PropsConst;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 
 public class DriverManager {
@@ -37,6 +42,21 @@ public class DriverManager {
         } else if (propManager.getProperty(PropsConst.TYPE_BROWSER).equals("chrome")) {
             System.setProperty("webdriver.chrome.driver", propManager.getProperty(PropsConst.PATH_CHROME_DRIVER_WINDOWS));
             driver = new ChromeDriver();
+        } else if (propManager.getProperty(PropsConst.TYPE_BROWSER).equals("remote")) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("chrome");
+            capabilities.setVersion("84.0");
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", false);
+
+            try {
+                driver = new RemoteWebDriver(
+                        URI.create("http://51.250.100.60:4444/wd/hub").toURL(),
+                        capabilities
+                );
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
