e4377b95ab87742518b10dd928163336aeae32f6
Johnny3kk <some@mail.ru>
2022-03-26 00:38:48 +0300

    Logger add;

--- src/main/java/ru/ibs/framework/steps/MortgagePageStep.java
+++ src/main/java/ru/ibs/framework/steps/MortgagePageStep.java
@@ -4,32 +4,41 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.ru.И;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.ibs.framework.managers.DriverManager;
 import ru.ibs.framework.managers.PageManager;
 
 public class MortgagePageStep {
 
+  private static final Logger logger = LoggerFactory.getLogger(MortgagePageStep.class);
+
   private final PageManager pageManager = PageManager.getInstance();
 
   @И("^Заполнить поля:$")
   public void fillFields(DataTable mapFieldsAndValue) {
+    logger.info("Начало заполнения полей");
     mapFieldsAndValue
         .asMap(String.class, String.class)
         .forEach(
             (key, value) -> pageManager.getMortgagePage().fillField((String) key, (String) value));
+    logger.info("Поля заполнены");
   }
 
   @И("^Убрать опцию 'Страхование жизни'$")
   public void disableInsuranceCheck() {
     pageManager.getMortgagePage().disableInsuranceCheck();
+    logger.info("Опция выключена");
   }
 
   @И("^Проверить значения полей:$")
   public void checkFields(DataTable mapFieldsAndValue) {
+    logger.info("Начало проверки полей");
     mapFieldsAndValue
         .asMap(String.class, String.class)
         .forEach(
             (key, value) ->
                 pageManager.getMortgagePage().assertCorrectData((String) key, (String) value));
+    logger.info("Проверка полей завершена");
   }
 }
--- src/main/java/ru/ibs/framework/steps/StartPageStep.java
+++ src/main/java/ru/ibs/framework/steps/StartPageStep.java
@@ -2,24 +2,32 @@ package ru.ibs.framework.steps;
 
 import io.cucumber.java.ru.И;
 import ru.ibs.framework.managers.PageManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StartPageStep {
+
+  private static final Logger logger = LoggerFactory.getLogger(StartPageStep.class);
 
   private final PageManager pageManager = PageManager.getInstance();
 
   @И("^Закрыть сообщение cookies$")
   public void closeCookieDialog() {
+    logger.info("Открытие стартовой страницы и закрытие всплывающего уведомления об использовании cookie");
     pageManager.getStartPage().checkOpenPage().closeCookieDialog();
+    logger.info("Закрыто уведомления об использовании cookie");
   }
 
   @И("^Выбираем \"(.+)\" в главном меню$")
   public void selectBaseMenu(String nameBaseMenu) {
       pageManager.getStartPage().selectMortgageMenu(nameBaseMenu);
+      logger.info("Нажата опция главного меню");
   }
 
   @И("^Выбираем \"(.+)\" в подменю главного меню$")
   public void selectSubMenu(String nameSubMenu) {
       pageManager.getStartPage().selectCreditForRealty(nameSubMenu);
+      logger.info("Нажата опция подменю");
   }
 
 }
